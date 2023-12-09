c3c9a8a4e4827a4be41922d79f4e3d472f89d7f1
LordBaryhobal <lordbaryhobal@gmail.com>
2023-12-09 11:53:51 +0100

    day 9 puzzle 1

---
+++ src/day9/Puzzle1.scala
@@ -0,0 +1,41 @@
+package day9
+
+import scala.collection.mutable
+import scala.io.{BufferedSource, Source}
+import scala.util.matching.Regex
+import scala.util.matching.Regex.Match
+
+object Puzzle1 {
+  var series: Array[Series] = Array.empty
+
+  def loadInput(path: String): Unit = {
+    val source: BufferedSource = Source.fromFile(path)
+    val lines: Array[String] = source.getLines().toArray
+
+    series = new Array(lines.length)
+
+    for ((line: String, i: Int) <- lines.zipWithIndex) {
+      series(i) = new Series(line.split(" ").map(_.toInt))
+    }
+
+    source.close()
+  }
+
+  def solve(path: String): Int = {
+    loadInput(path)
+
+    var solution: Int = 0
+
+    for (s: Series <- series) {
+      s.computeDiffs()
+      solution += s.extrapolate()
+    }
+
+    return solution
+  }
+
+  def main(args: Array[String]): Unit = {
+    val solution: Int = solve("./res/day9/input1.txt")
+    println(solution)
+  }
+}
---
+++ src/day9/Series.scala
@@ -0,0 +1,30 @@
+package day9
+
+import scala.collection.mutable.ArrayBuffer
+
+class Series {
+  private val values: ArrayBuffer[Array[Int]] = new ArrayBuffer()
+  def this(initialValues: Array[Int]) = {
+    this()
+    values.addOne(initialValues.concat(Array(0)))
+  }
+
+  def computeDiffs(): Unit = {
+    while (!(values.last.dropRight(1).distinct.length == 1 && values.last(0) == 0)) {
+      val diffs: Array[Int] = new Array(values.last.length-1)
+      for (i: Int <- 0 until diffs.length-1) {
+        diffs(i) = values.last(i+1) - values.last(i)
+      }
+      values.addOne(diffs)
+    }
+  }
+
+  def extrapolate(): Int = {
+    val len: Int = values(0).length
+    values.last(values.last.length-1) = 0
+    for (i: Int <- values.length-2 to 0 by -1) {
+      values(i)(len-i-1) = values(i)(len-i-2) + values(i+1)(len-i-2)
+    }
+    return values(0).last
+  }
+}
